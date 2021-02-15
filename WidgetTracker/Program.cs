5ec8cb52bf05d5e1507caef2fb311cf7f97ac497
Jada <jada.fagan@gmail.com>
2021-02-15 14:34:28 -0500

    Add project files.

---
+++ WidgetTracker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WidgetTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var w1 = new Widget("Large", "red");
+            Console.WriteLine($"W1 size: {w1.Size} color: {w1.Color}, Costs ${w1.Price()}");
+        }
+    }
+}
---
+++ WidgetTracker/Widget.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WidgetTracker
+{
+    class Widget
+    {
+
+        public string Size { get; set; }
+        public string Color { get; set; }
+
+
+
+        public int Price()
+        {
+            var price = 0;
+            if(Size== "large")
+            {
+                price += 20;
+            }
+            if(Size == "medium")
+            {
+                price += 10;
+            }
+            if(Size == "small")
+            {
+                price += 5;
+            }
+            if(Color == "red")
+            {
+                price += 15;
+            }
+            if(Color == "white")
+            {
+                price += 10;
+            }
+            if(Color == "blue")
+            {
+                price += 5;
+            }
+
+            return price;
+        }
+
+        public Widget()
+        {
+
+        }
+
+        public Widget(string size, string color)
+        {
+            Size = size.ToLower();
+            Color = color.ToLower();
+
+        }
+    }
+}
